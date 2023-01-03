8b9748a949ab077af44354b15d8633abe456d75c
jasonleung111 <j.leung@sleekflow.io>
2023-01-03 17:47:55 +0800

    Impl fade in out effect

--- pages/components/skills/bubble.tsx
+++ pages/components/skills/bubble.tsx
@@ -5,33 +5,19 @@ type Props = {
     percent: String,
 }
 
-function FrontSide(name: String) {
+export default function SkillBubble({ name, percent }: Props) {
     return (
-        <div className='border-solid border-white border-[1px] rounded-full h-[100px] w-[100px] md:h-[130px] md:w-[130px] lg:h-[150px] lg:w-[150px] flex justify-center items-center bg-background-dark'>
-            <div className='flex items-center justify-center w-[100px] md:w-[130px] lg:w-[150px] text-[18px] md:text-[24px] lg:text-[30px] text-center'>
-                {name}
+        <div className='relative'>
+            <div className='absolute border-solid border-white border-[1px] rounded-full h-[100px] w-[100px] md:h-[130px] md:w-[130px] lg:h-[150px] lg:w-[150px] flex justify-center items-center bg-background-dark opacity-100 hover:opacity-0 transition-opacity duration-500'>
+                <div className='flex items-center justify-center w-[100px] md:w-[130px] lg:w-[150px] text-[18px] md:text-[24px] lg:text-[30px] text-center'>
+                    {name}
+                </div>
             </div>
-        </div>
-    );
-}
-
-function BackSide(percent: String) {
-    return (
-        <div className=''>
             <div className='border-solid border-white border-[1px] rounded-full h-[100px] w-[100px] md:h-[130px] md:w-[130px] lg:h-[150px] lg:w-[150px] flex justify-center items-center bg-background-dark'>
                 <div className='flex items-center justify-center w-[100px] md:w-[130px] lg:w-[150px] text-[18px] md:text-[24px] lg:text-[30px] text-center'>
                     {percent}%
                 </div>
             </div>
-        </div>
-    );
-}
-
-export default function SkillBubble({ name, percent }: Props) {
-    return (
-        <div className="relative">
-            {FrontSide(name)}
-            {BackSide(percent)}
         </div>
     )
 }
